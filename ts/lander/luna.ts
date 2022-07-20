d40994b6cba7b60a07a3e39858067eefe39fe48e
Matt Deeds <mdeeds@gmail.com>
2022-07-19 20:14:24 -0700

    bob to lift

--- ts/lander/luna.ts
+++ ts/lander/luna.ts
@@ -1,21 +1,22 @@
 import * as THREE from "three";
+import { Object3D } from "three";
 import { Tick, Ticker } from "../ticker";
 import { World } from "../world";
 import { ThirdPersonCamera } from "./thirdPersonCamera";
 import { Zoa } from "./zoa";
 
 export class Luna extends THREE.Object3D implements World, Ticker {
+  private cameraPos = new Object3D();
+  private zoa = new Zoa();
   constructor(private camera: THREE.PerspectiveCamera,
     private renderer: THREE.WebGLRenderer) {
     super();
-
+    camera.add(this.cameraPos);
     const light = new THREE.HemisphereLight('#fff', '#222');
     this.add(light);
-
-    const zoa = new Zoa();
-    zoa.position.set(0, 0.9, -2.0);
-    const tpc = new ThirdPersonCamera(zoa);
-    this.add(zoa);
+    this.zoa.position.set(0, 0.9, -2.0);
+    const tpc = new ThirdPersonCamera(this.zoa);
+    this.add(this.zoa);
   }
 
   run(): Promise<string> {
@@ -25,7 +26,21 @@ export class Luna extends THREE.Object3D implements World, Ticker {
   }
 
   private spacePressed = false;
+  private previousY = undefined;
+  private currentCameraPosition = new THREE.Vector3();
   tick(t: Tick) {
+    this.cameraPos.getWorldPosition(this.currentCameraPosition);
+    if (this.previousY) {
+      const deltaY = this.currentCameraPosition.y - this.previousY;
+      if (deltaY > 0) {
+        this.zoa.position.y += 2.0 * deltaY;
+      }
+    }
+    this.zoa.position.y -= 0.3 * t.deltaS;
+    if (this.zoa.position.y < 0.0) {
+      this.zoa.position.y = 0.0;
+    }
+    this.previousY = this.currentCameraPosition.y;
   }
 
 }
